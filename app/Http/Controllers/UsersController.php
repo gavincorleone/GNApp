e75028e4c2abe3c1bd3aec207ecf9d3cc04a4411
gavincorleone <gavin_corleone@163.com>
2019-02-22 14:07:20 +0800

    用户显示界面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     //
     public function create()
     {
-        return view('users.create');
+        return view('users/create');
+    }
+    public function show(User $user)
+    {
+//        compact 是将user model转化为一个关联数组，然后跟视图绑定
+        return view('users/show',compact('user'));
     }
 }
