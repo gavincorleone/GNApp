11b75def7a0b124f5176ddccc0b23640bb9088fd
gavincorleone <gavin_corleone@163.com>
2019-02-22 17:08:03 +0800

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -14,10 +14,11 @@ class SessionsController extends Controller
     }
     public function store(Request $request)
     {
-       $credentials = $this->validate($request,[
-           'email' => 'required|email|max:255',
-           'password' => 'required'
-       ]);
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
         if (Auth::attempt($credentials)) {
             session()->flash('success', '欢迎回来！');
             return redirect()->route('users.show', [Auth::user()]);
@@ -25,6 +26,11 @@ class SessionsController extends Controller
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back()->withInput();
         }
-       return;
+    }
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
     }
 }
