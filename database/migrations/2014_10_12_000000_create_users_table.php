92236f837dc41672bcec3f68136aa787bb9e9ce9
gavincorleone <gavin_corleone@163.com>
2019-02-27 11:38:38 +0800

    管理员可以删除用户

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -31,6 +31,9 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            //
+            Schema::dropIfExists('users');
+        });
     }
 }
