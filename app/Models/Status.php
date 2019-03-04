5a135f1512d7f2d4cb91e68e03c629e45c7ff4de
gavincorleone <gavin_corleone@163.com>
2019-03-04 14:50:03 +0800

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
-    //
-//   反向关联，通过微博查询用户
+    protected $fillable = ['content'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
