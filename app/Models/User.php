aca55f3765af2bb54d969fe37480ca3649bfe72e
gavincorleone <gavin_corleone@163.com>
2019-03-04 15:51:24 +0800

    首页微博列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,21 +27,31 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
     public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
+
     public static function boot()
     {
         parent::boot();
-        static::creating(function ($user){
+        static::creating(function ($user) {
             $user->activation_token = str_random(30);
         });
     }
+
 //    一个用户有多条微博
     public function statuses()
     {
         return $this->hasMany(Status::class);
     }
+
+    public function feed()
+    {
+        return $this->statuses()
+            ->orderBy('created_at', 'desc');
+    }
+
 }
